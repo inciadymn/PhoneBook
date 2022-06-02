ab5695a6d5ca73475b286c3f94026dcf075e674c
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-06-02 13:20:51 +0300

    Contact unit test yazıldı.

--- PhoneBook.Test/ControllerTests/ContactControllerTests.cs
+++ PhoneBook.Test/ControllerTests/ContactControllerTests.cs
@@ -6,6 +6,7 @@ using PhoneBook.BLL.Abstract;
 using PhoneBook.BLL.Concrete.ResultServiceBLL;
 using PhoneBook.Model.Dto;
 using PhoneBook.Model.Enums;
+using System.Collections.Generic;
 using Xunit;
 
 namespace PhoneBook.Test.ControllerTests
@@ -13,7 +14,7 @@ namespace PhoneBook.Test.ControllerTests
     public class ContactControllerTests
     {
         [Fact]
-        public void ContactController_Insert_ShouldReturnOk_WithExpectedValues()
+        public void ContactController_Insert_ShouldReturnOk()
         {
             //Arrange
             var contactServiceMock = new Mock<IContactService>();
@@ -29,7 +30,7 @@ namespace PhoneBook.Test.ControllerTests
                 .Returns(new ResultService<ContactCreateDto>()
                 {
                     Data = request,
-                    HasError = false
+                    HasError = false,
                 });
 
             var contactController = CreateInstance(contactServiceMock);
@@ -39,6 +40,98 @@ namespace PhoneBook.Test.ControllerTests
 
             //Assert
             Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void ContactController_Insert_ShouldReturnBadRequest()
+        {
+            //Arrange
+            var contactServiceMock = new Mock<IContactService>();
+
+            var request = new ContactCreateDto()
+            {
+                InfoContent = "istanbul",
+                InfoType = "abc"
+            };
+
+            contactServiceMock
+                .Setup(a => a.Insert(request, 1))
+                .Returns(new ResultService<ContactCreateDto>()
+                {
+                    Data = null,
+                    HasError = true,
+                    Errors = new List<ErrorItem>()
+                    {
+                       new ErrorItem()
+                       {
+                           ErrorType="Insert Error",
+                           ErrorMessage ="InfoType is not valid"
+                       }
+                    }
+                });
+
+            var contactController = CreateInstance(contactServiceMock);
+
+            //Act
+            var result = contactController.Insert(request, 1);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void ContactController_Delete_ShouldReturnOk()
+        {
+            //Arrange
+            var contactServiceMock = new Mock<IContactService>();
+
+            contactServiceMock
+                .Setup(a => a.Delete(1))
+                .Returns(new ResultService<bool>()
+                {
+                    Data = true,
+                    HasError = false
+                });
+
+            var contactController = CreateInstance(contactServiceMock);
+
+            //Act
+            var result = contactController.Delete(1);
+
+            //Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void ContactController_Report_ShouldReturnOk()
+        {
+            //Arrange
+            var contactServiceMock = new Mock<IContactService>();
+
+            contactServiceMock
+                .Setup(a => a.GetReport())
+                .Returns(new ResultService<List<ReportDto>>()
+                {
+                    Data = new List<ReportDto>()
+                    {
+                        new ReportDto()
+                        {
+                            Location = "istanbul",
+                            LocationCount = 1,
+                            NumberCount = 1,
+                            UserCount = 1
+                        }
+                    },
+                    HasError = false
+                });
+
+            var contactController = CreateInstance(contactServiceMock);
+
+            //Act
+            var result = contactController.Report();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
         }
 
         private ContactController CreateInstance(Mock<IContactService> contactServiceMock)
--- PhoneBook.Test/ControllerTests/UserControllerTests.cs
+++ PhoneBook.Test/ControllerTests/UserControllerTests.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PhoneBook.API.Controllers;
+using PhoneBook.BLL.Abstract;
+using PhoneBook.BLL.Concrete.ResultServiceBLL;
+using PhoneBook.Model.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace PhoneBook.Test.ControllerTests
 {
-    class UserControllerTests
+    public class UserControllerTests
     {
+        [Fact]
+        public void UserController_Insert_ShouldReturnOk()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+
+            var request = new UserDto()
+            {
+                FirstName = "test",
+                LastName = "test",
+                Company = "test"
+            };
+
+            userServiceMock
+                .Setup(a => a.Insert(request))
+                .Returns(new ResultService<UserDto>()
+                {
+                    Data = request,
+                    HasError = false
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            //Act
+            var result = userController.Insert(request);
+
+            //Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        private UserController CreateInstance(Mock<IUserService> userServiceMock)
+        {
+            var userController = new UserController(userServiceMock.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            return userController;
+        }
     }
 }
