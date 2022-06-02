43996740d69e5f12efb1e8c049942ce213837b0c
İnci Adıyaman <inci.adiyaman@hotmail.com>
2022-06-02 17:10:30 +0300

    UserController için unit test yazıldı

--- PhoneBook.Test/ControllerTests/UserControllerTests.cs
+++ PhoneBook.Test/ControllerTests/UserControllerTests.cs
@@ -44,7 +44,151 @@ namespace PhoneBook.Test.ControllerTests
 
             //Assert
             Assert.IsType<OkResult>(result);
+        }      
+
+        [Fact]
+        public void UserController_Insert_ShouldReturnBadRequest()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+
+            var request = new UserDto()
+            {
+                FirstName = "test",
+                LastName = "test",
+                Company = "test"
+            };
+
+            userServiceMock
+                .Setup(a => a.Insert(request))
+                .Returns(new ResultService<UserDto>()
+                {
+                    Data = null,
+                    HasError = true,
+                    Errors = new List<ErrorItem>()
+                    {
+                        new ErrorItem()
+                        {
+                            ErrorType="Insert Error",
+                            ErrorMessage ="Add not successful",
+                        }
+                    }
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            //Act
+            var result = userController.Insert(request);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
         }
+
+        [Fact]
+        public void UserController_Delete_ShouldReturnOk()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>(); ;
+
+            userServiceMock
+                .Setup(a => a.Delete(1))
+                .Returns(new ResultService<bool>()
+                {
+                    Data = true,
+                    HasError=false,
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            //Act
+            var result = userController.Delete(1);
+
+            //Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void UserController_Update_ShouldReturnOk()
+        {
+            var userServiceMock = new Mock<IUserService>();
+
+            var request = new UserDto()
+            {
+                FirstName = "test",
+                LastName = "test",
+                Company = "test"
+            };
+
+            userServiceMock
+                .Setup(a => a.Update(request, 1))
+                .Returns(new ResultService<bool>()
+                {
+                    Data = true,
+                    HasError = false,
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            var result = userController.Update(request, 1);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public void UserController_GetAllUsers_ShouldReturnOk()
+        {
+            var userServiceMock = new Mock<IUserService>();
+
+            userServiceMock
+                .Setup(a => a.GetAllUsers())
+                .Returns(new ResultService<List<GetAllUserDto>>()
+                {
+                    Data = new List<GetAllUserDto>()
+                    {
+                        new GetAllUserDto()
+                        {
+                            ID=1,
+                            FirstName="test",
+                            LastName="test",
+                            Company="test"
+                        }
+                    },
+                    HasError = false,
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            var result = userController.GetAll();
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void UserController_GetUserContacts_ShouldReturnOk()
+        {
+            var userServiceMock = new Mock<IUserService>();
+
+            userServiceMock
+                .Setup(a => a.GetUserContacts(1))
+                .Returns(new ResultService<GetUserContactsDto>()
+                {
+                    Data = new GetUserContactsDto() 
+                    {
+                        ID=1,
+                        FirstName="test",
+                        LastName="test",
+                        Company="test"
+                    },
+                    HasError = false
+                });
+
+            var userController = CreateInstance(userServiceMock);
+
+            var result = userController.GetUserContacts(1);
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
 
         private UserController CreateInstance(Mock<IUserService> userServiceMock)
         {
